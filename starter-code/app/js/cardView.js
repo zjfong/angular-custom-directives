44e78938a7d01916a820ba975e6b7e84e113a94a
Ben Hulan <benhulan@gmail.com>
2015-12-04 12:03:48 -0800

    initial branch commit

---
+++ starter-code/app/js/cardView.js
@@ -0,0 +1,13 @@
+angular.module('CardsAgainstAssembly')
+    .directive('card', cardView);
+
+function cardView(){
+    var directive = {};
+    directive.restrict = "E";
+    directive.templateUrl = '_cardView.html';
+    directive.scope = {
+        question: '@',
+        answer: '@'
+    }
+    return directive;
+}
