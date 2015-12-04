e2724b9603a2c6fb47ff7c6212d44142a001bc7b
Ben Hulan <benhulan@gmail.com>
2015-12-04 12:30:40 -0800

    moves solution to solution-code dir

--- solution-code/app/js/cardView.js
+++ solution-code/app/js/cardView.js
@@ -1,15 +1,14 @@
 angular.module('CardsAgainstAssembly')
-  .directive('card', cardView);
+    .directive('card', cardView);
 
 function cardView(){
-  var directive = {};
-
-  //'A' == attribute, 'E' == element, 'C' == class
-  directive.restrict = 'E';
-  directive.replace = true;
-  directive.templateUrl =  "_cardView.html";
-  directive.scope = {
-      question: '@'
-  };
-  return directive;
+    var directive = {};
+    directive.restrict = "E";
+    directive.templateUrl = '_cardView.html';
+    directive.scope = {
+        question: '@',
+        answer: '@',
+        'swapMe':'&swapMe'
+    }
+    return directive;
 }
--- solution-code/app/js/cardsController.js
+++ solution-code/app/js/cardsController.js
@@ -1,15 +1,39 @@
 angular.module('CardsAgainstAssembly')
-  .controller('cardsController', cardsController);
+  .controller('cardsController', ['$scope', function($scope){
 
-function cardsController(){
+  $scope.swapMe = function(){
+    console.log(this.$$watchers[0].last);
+  }
+
   var self = this;
   self.all = [
-    {question: "What is Batman's guilty pleasure?"},
-    {question: "I'm sorry professor, I couldn't complete my homework because _________."},
-    {question: "I get by with a little help from _________."},
-    {question: "_________. It's a trap!"},
-    {question: "The class field trip was completely ruined by _________."},
-    {question: "What's my secret power?"}
+    {
+      question: "What is Batman's guilty pleasure?", 
+      answer: "Robert Downey Jr."
+    },
+    {
+      question: "I'm sorry professor, I couldn't complete my homework because _________.",
+      answer: "the dog ate my assignment!"
+    },
+    {
+      question: "I get by with a little help from _________.",
+      answer: "John Cena!"
+    },
+    {
+      question: "_________ -- It's a trap!",
+      answer: "General Assembly?"
+    },
+    {
+      question: "The class field trip was completely ruined by _________.",
+      answer: "dragons"
+    },
+    {
+      question: "What's my secret power?",
+      answer: "Angular Baby!"
+    },
+    {
+      question: "Why are there so many songs about rainbows?",
+      answer: "It's not that easy being green."
+    }
   ]
-  return self;
-}
+}]);
